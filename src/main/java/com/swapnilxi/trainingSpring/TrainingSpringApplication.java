3d1eaef298cb5f9ec9ff721e6c8a0c4e50462b1b
swapnil <swapnilg671@gmail.com>
2023-02-06 19:09:41 +0530

    added project

---
+++ src/main/java/com/swapnilxi/trainingSpring/TrainingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.trainingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapnilxi/trainingSpring/TrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.trainingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
